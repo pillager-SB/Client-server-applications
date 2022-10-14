c8f0e9de8d1177acaf8093160bcb50010250c2dd
pillager-SB <78688006+pillager-SB@users.noreply.github.com>
2022-10-14 17:49:39 +0300

    Logging

---
+++ practice_05/client.py
@@ -0,0 +1,84 @@
+import argparse
+import json
+import sys
+import time
+import socket
+from common.variables import *
+from common.utils import send_message, get_message
+import logging
+import logs.config_client_log
+from errors import ReqFieldMissingError
+
+CLIENT_LOGGER = logging.getLogger('client')
+
+
+def create_presence(account_name='Guest'):
+    """Функция создает словарь для запроса о присутствии клиента"""
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    CLIENT_LOGGER.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
+    return out
+
+
+def process_ans(message):
+    """Функция разбирающая ответ сервера"""
+    CLIENT_LOGGER.debug(f'Разбор ответа сервера: {message}')
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : Ok!'
+        return f'400 : {message[ERROR]}'
+    raise ReqFieldMissingError(RESPONSE)
+
+
+def main():
+    """
+    Загрузка параметров из командной строки,
+    при их отсутствии - обработка значений, принятых по умолчанию.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, help='Reading an IP address')
+    parser.add_argument('port', type=int, default=DEFAULT_PORT, help='Read port IP address')
+    args = parser.parse_args()
+    server_address = args.addr
+    server_port = args.port
+    if server_port < 1024 or server_port > 65535:
+        CLIENT_LOGGER.critical(
+            f'Номер {server_port} не является приемлемым номером порта. '
+            f'Допустимы номера в диапазоне 1024-65535. '
+            f'Клиент будет завершен.')
+        sys.exit(1)
+
+    CLIENT_LOGGER.info(f'Запущен клиент с параметрами: адрес сервера: {server_address}, порт: {server_port}.')
+
+    #  Инициализация сокета и обмен.
+    try:
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.connect((server_address, server_port))
+        message_to_server = create_presence()
+        send_message(transport, message_to_server)
+        answer = process_ans(get_message(transport))
+
+    except json.JSONDecodeError:
+        CLIENT_LOGGER.error('Не удалось декодировать сообщение сервера.')
+    except ReqFieldMissingError as missing_error:
+        CLIENT_LOGGER.error(f'В ответе сервера отсутствует необходимое поле '
+                            f'{missing_error.missing_field}')
+    except ConnectionRefusedError:
+        CLIENT_LOGGER.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+                               f'конечный компьютер отверг запрос на подключение.')
+    except TimeoutError:
+        CLIENT_LOGGER.critical(f'Попытка установить соединение была безуспешной, т.к. от '
+                               f'{server_address}:{server_port} за требуемое время не получен нужный отклик, '
+                               f'или было разорвано уже установленное соединение из-за неверного отклика '
+                               f'уже подключенного компьютера')
+    else:
+        CLIENT_LOGGER.info(f'Принят ответ: {answer} от сервера {server_address}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ practice_05/server.py
@@ -0,0 +1,78 @@
+import argparse
+import json
+import sys
+import socket
+import logging
+import logs.config_server_log
+from common.variables import *
+from errors import IncorrectDataRecivedError
+from common.utils import send_message, get_message
+
+SERVER_LOGGER = logging.getLogger('server')
+
+
+def process_client_message(message):
+    """
+    Функция-обработчик сообщений от клиентов, принимает словарь-сообщение,
+    проверяет корректность, возвращает словарь-ответ для клиента.
+    """
+    SERVER_LOGGER.debug(f'Разбор сообщения от клиента : {message}')
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+        return {RESPONSE: 200}  # Запрос выполнен успешно.
+    return {
+        RESPONSE: 400,  # Сервер не смог понять запрос.
+        ERROR: 'Bad Request'
+    }
+
+
+def main():
+    """
+    Загрузка параметров из командной строки,
+    при их отсутствии - обработка значений, принятых по умолчанию.
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--addr', default='', help='Reading an IP address')
+    parser.add_argument('-p', '--port', default=DEFAULT_PORT, type=int, help='Read port IP address')
+    args = parser.parse_args()
+    listen_address = args.addr
+    listen_port = args.port
+
+    # Проверка получения корректного номера порта для работы сервера.
+    if listen_port < 1024 or listen_port > 65535:
+        SERVER_LOGGER.critical(
+            f'Номер {listen_port} не является приемлемым номером порта. '
+            f'Допустимы номера в диапазоне 1024-65535.')
+        sys.exit(1)
+    SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
+                       f'адрес с которого принимаются подключения: {listen_address}. '
+                       f'Если адрес не указан, принимаются соединения с любых адресов.')
+
+    # Готовим сокет.
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    transport.bind((listen_address, listen_port))
+
+    # Слушаем порт.
+    transport.listen(MAX_CONNECTION)
+
+    while True:
+        client, client_address = transport.accept()
+        SERVER_LOGGER.info(f'Установлено соединение с ПК {client_address}')
+        try:
+            message_from_client = get_message(client)
+            SERVER_LOGGER.debug(f'Получено сообщение {message_from_client}')
+            response = process_client_message(message_from_client)
+            SERVER_LOGGER.info(f'Сформирован ответ клиенту {response}')
+            send_message(client, response)
+            SERVER_LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
+            client.close()
+        except json.JSONDecodeError:
+            SERVER_LOGGER.error(f'Не удалось декодировать сообщение от клиента {client_address}. '
+                                f'Соединение будет закрыто')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
